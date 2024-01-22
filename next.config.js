85b1b758df3ce626c76d244a2c1119e48c4360dc
Rafael Durelli <rafael.durelli@ufla.br>
2024-01-22 18:23:03 -0300

    feat: first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  output: "standalone",
+  env: {
+    NEXT_AWS_S3_REGION: process.env.NEXT_AWS_S3_REGION,
+    NEXT_AWS_S3_ACCESS_KEY_ID: process.env.NEXT_AWS_S3_ACCESS_KEY_ID,
+    NEXT_AWS_S3_SECRET_ACCESS_KEY: process.env.NEXT_AWS_S3_SECRET_ACCESS_KEY,
+    NEXT_AWS_S3_BUCKET_NAME: process.env.NEXT_AWS_S3_BUCKET_NAME,
+  }
+};
--- src/app/upload/(form)/actions.js
+++ src/app/upload/(form)/actions.js
@@ -1,7 +1,8 @@
 "use server";
 import { revalidatePath } from "next/cache";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import sharp from "sharp";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
 
 const s3Client = new S3Client({
   region: process.env.NEXT_AWS_S3_REGION,
@@ -13,10 +14,12 @@ const s3Client = new S3Client({
 
 
 async function uploadFileToS3(file, fileName) {
-  const fileBuffer = await sharp(file)
-  .jpeg({quality: 50})
-  .resize(800, 400)
-  .toBuffer();
+  const fileBuffer = file
+
+  // console.log("s3Client " + s3Client.credentials.accessKeyId)
+
+  // console.log("Chamou o uploadFileToS3" + file)
+  // console.log(fileName)
 
   const params = {
     Bucket: process.env.NEXT_AWS_S3_BUCKET_NAME,
@@ -25,14 +28,30 @@ async function uploadFileToS3(file, fileName) {
     ContentType: "image/jpg",
   };
 
+  console.log("Params " + params.Bucket)
+  console.log("Params " + params.Bucket)
+
   const command = new PutObjectCommand(params);
-  try {
-    const response = await s3Client.send(command);
-    console.log("File uploaded successfully:", response);
-    return fileName;
-  } catch (error) {
-    throw error;
-  }
+
+  const url = await getSignedUrl(s3Client, command, { expiresIn: 360 })// 5 minutes
+
+  console.log("url " + url)
+
+  await fetch(url, {
+    method: "PUT",
+    body: fileBuffer,
+    headers: {
+      "Content-Type": file.type,
+    },
+  });
+
+  // try {
+  //   const response = await s3Client.send(command);
+  //   console.log("File uploaded successfully:", response);
+  //   return fileName;
+  // } catch (error) {
+  //   throw error;
+  // }
 }
 
 export async function uploadFile(prevState, formData) {
@@ -49,6 +68,6 @@ export async function uploadFile(prevState, formData) {
     revalidatePath("/");
     return { status: "success", message: "File has been upload." };
   } catch (error) {
-    return { status: "error", message: "Failed to upload file." };
+    return { status: "error", message: "Failed to upload file." + error };
   }
 }
