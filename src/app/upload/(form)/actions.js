497328822d1c5465517984bd37c59aa613829d70
Irineu Marques <mririneu.dev@gmail.com>
2024-01-24 09:45:11 -0300

    feature: multipart upload

--- src/app/upload/(form)/actions.js
+++ src/app/upload/(form)/actions.js
@@ -1,8 +1,6 @@
 "use server";
 import { revalidatePath } from "next/cache";
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-
+import { S3Client, CreateMultipartUploadCommand, UploadPartCommand, CompleteMultipartUploadCommand } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
   region: process.env.NEXT_AWS_S3_REGION,
@@ -12,46 +10,73 @@ const s3Client = new S3Client({
   },
 });
 
-
-async function uploadFileToS3(file, fileName) {
-  const fileBuffer = file
-
-  // console.log("s3Client " + s3Client.credentials.accessKeyId)
-
-  // console.log("Chamou o uploadFileToS3" + file)
-  // console.log(fileName)
-
+async function initiateMultipartUpload(fileName) {
   const params = {
     Bucket: process.env.NEXT_AWS_S3_BUCKET_NAME,
-    Key: `${fileName}`,
-    Body: fileBuffer,
+    Key: fileName,
     ContentType: "image/jpg",
   };
 
-  console.log("Params " + params.Bucket)
-  console.log("Params " + params.Bucket)
+  const createMultipartUploadCommand = new CreateMultipartUploadCommand(params);
 
-  const command = new PutObjectCommand(params);
+  const { UploadId } = await s3Client.send(createMultipartUploadCommand);
 
-  const url = await getSignedUrl(s3Client, command, { expiresIn: 360 })// 5 minutes
+  return UploadId;
+}
 
-  console.log("url " + url)
+async function uploadPart(uploadId, partNumber, fileBuffer, fileName) {
+  const params = {
+    Bucket: process.env.NEXT_AWS_S3_BUCKET_NAME,
+    Key: fileName,
+    PartNumber: partNumber,
+    UploadId: uploadId,
+    Body: fileBuffer,
+  };
 
-  await fetch(url, {
-    method: "PUT",
-    body: fileBuffer,
-    headers: {
-      "Content-Type": file.type,
-    },
-  });
+  const uploadPartCommand = new UploadPartCommand(params);
 
-  // try {
-  //   const response = await s3Client.send(command);
-  //   console.log("File uploaded successfully:", response);
-  //   return fileName;
-  // } catch (error) {
-  //   throw error;
-  // }
+  const { ETag } = await s3Client.send(uploadPartCommand);
+
+  return { PartNumber: partNumber, ETag };
+}
+
+async function completeMultipartUpload(uploadId, fileName, parts) {
+  const params = {
+    Bucket: process.env.NEXT_AWS_S3_BUCKET_NAME,
+    Key: fileName,
+    UploadId: uploadId,
+    MultipartUpload: { Parts: parts },
+  };
+
+  const completeMultipartUploadCommand = new CompleteMultipartUploadCommand(params);
+
+  await s3Client.send(completeMultipartUploadCommand);
+}
+
+async function uploadFileToS3(file, fileName) {
+  const fileBuffer = file;
+
+  const uploadId = await initiateMultipartUpload(fileName);
+  const partSize = 5 * 1024 * 1024;
+
+  const fileParts = [];
+  let start = 0;
+  let partNumber = 1;
+
+  while (start < fileBuffer.length) {
+    const end = Math.min(start + partSize, fileBuffer.length);
+    const partBuffer = fileBuffer.slice(start, end);
+
+    console.log(`Uploading part ${partNumber}/${Math.ceil(fileBuffer.length / partSize)}`);
+
+    const { ETag } = await uploadPart(uploadId, partNumber, partBuffer, fileName);
+    fileParts.push({ PartNumber: partNumber, ETag });
+
+    start = end;
+    partNumber += 1;
+  }
+
+  await completeMultipartUpload(uploadId, fileName, fileParts);
 }
 
 export async function uploadFile(prevState, formData) {
@@ -66,7 +91,7 @@ export async function uploadFile(prevState, formData) {
     await uploadFileToS3(buffer, file.name);
 
     revalidatePath("/");
-    return { status: "success", message: "File has been upload." };
+    return { status: "success", message: "File has been uploaded." };
   } catch (error) {
     return { status: "error", message: "Failed to upload file." + error };
   }
